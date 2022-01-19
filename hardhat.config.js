d632dc8f5781cbab30aa7774ae9e35ec15205e81
darah3ng <daragitgud@gmail.com>
2022-01-19 14:46:08 +1000

    chore: modify hardhat configure

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,5 +17,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.3",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
+  }
 };
