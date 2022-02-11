13fbca10ef2074ac745f14bef658bcb54508c955
darah3ng <daragitgud@gmail.com>
2022-02-11 14:40:28 +1000

    feat: display dark mode by default

--- src/components/layouts/Mainlayout.jsx
+++ src/components/layouts/Mainlayout.jsx
@@ -1,8 +1,19 @@
-import { Flex, Box } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { Flex, Box, useColorMode } from '@chakra-ui/react';
+import customTheme from '../../theme/customTheme';
 
 const Layout = ({ children }) => {
+  const { colorMode, setColorMode } = useColorMode();
+
+  // set dark mode by default
+  useEffect(() => {
+    setColorMode('dark');
+  }, [setColorMode]);
+
+  const brandColorTheme = customTheme.colors.mode[colorMode || 'light'];
+
   return (
-    <Box minH={'90vh'} mt={12}>
+    <Box minH={'90vh'} mt={12} background={brandColorTheme}>
       <Flex
         direction='column'
         align='center'
